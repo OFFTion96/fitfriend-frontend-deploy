e5597be60185fb14cbdff2c59f0286f4cc5a1772
apisakch <apisakch11@hotmail.com>
2022-08-24 20:09:37 +0700

    edit

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 const config = {
-  // url:"https://fitfriend-backend-deploy.vercel.app"
+  url:"https://fitfriend-backend-deploy.vercel.app"
   // url:"http://localhost:9000"
-  url:"https://backend-test-vercel-offtion96.vercel.app"
+  // url:"https://backend-test-vercel-offtion96.vercel.app"
     }
 
 export default config
